49bf08f9c9a1550cd13f8822cd784b4d85bc967b
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-03-17 15:31:06 +0530

    feat: add initial authentication routes setup

---
+++ server/routes/auth.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/register");
+router.post("/login");
+
+router.get("/me");
+router.put("/profile");
+router.put("/change-password");
+router.post("/logout");
+
+module.exports=router;
