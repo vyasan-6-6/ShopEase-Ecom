c61d052a0353653304a1990a485b5316f5b0ab9a
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-03-25 16:05:38 +0530

    feat: setup frontend routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
- 
-import './App.css'
-
+import "./App.css";
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 function App() { 
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/auth/register" element={<Register/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
