3746332e8d8f52fad7099ab961ee7e57cb7ac989
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-03-27 11:12:45 +0530

    feat: setup redux structure with custom hook re-exports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Register from "./pages/Register";
+import Register from "./pages/auth/Register";
 function App() { 
   return (
     <BrowserRouter>
