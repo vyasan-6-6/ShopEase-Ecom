cd5beacfc5a7d91f680643a0d0ccbb4fbd2d46d4
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-03-27 22:07:44 +0530

    feat: implement OTP verification thunk and add cases in auth slice

--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -1,51 +1,69 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"; 
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { authAPI } from "../../../services";
 
-export const registerUser = createAsyncThunk("auth/registerUser",async (data,{rejectWithValue})=>{
+export const registerUser = createAsyncThunk("auth/registerUser", async (data, { rejectWithValue }) => {
     try {
         await authAPI.resgister(data);
-        return {email:data.email} ;
+        return { email: data.email };
+    } catch (error) {
+        return rejectWithValue(error.response?.data.message || error.message);
+    }
+});
+
+export const verifyOtp = createAsyncThunk("auth/verifyOtp", async ({ email, otp }, { rejectWithValue }) => {
+    try {
+        const res = await authAPI.verifyRegisterOtp({ email, otp });
+        return res; //{user,token} present
     } catch (error) {
         return rejectWithValue(error.response?.data.message || error.message);
     }
 });
 export const authSlice = createSlice({
-    name:"auth",
-    initialState:{
-        user:null,
-        loading:false,
-        error:null,
-        step:"form",
-        email:null,
-        cooldown:0
+    name: "auth",
+    initialState: {
+        user: null,
+        loading: false,
+        error: null,
+        step: "form",
+        email: null,
+        cooldown: 0,
     },
-    reducers:{
-        clearError:(state)=>{
-            state.error = null
+    reducers: {
+        clearError: (state) => {
+            state.error = null;
         },
-startCooldown:(state)=>{
-    state.cooldown=60
-},
-tickCooldown:(state)=>{
-    if(state.cooldown>0)state.cooldown -=1;
-},
-resetAuthFlow:(state)=>{
-state.step="form",
-state.email=null,
-state.cooldown = 0
-}
+        startCooldown: (state) => {
+            state.cooldown = 60;
+        },
+        tickCooldown: (state) => {
+            if (state.cooldown > 0) state.cooldown -= 1;
+        },
+        resetAuthFlow: (state) => {
+            ((state.step = "form"), (state.email = null), (state.cooldown = 0));
+        },
     },
-    extraReducers:(builder)=>{
-builder
-.addCase(registerUser.pending,(state)=>{
-    state.loading = true,
-    state.error = null
-})
-.addCase(registerUser.fulfilled,(state,action)=>{
-state.loading = false,
-state.step= "otp",
-state.cooldown = 60,
-state.email = action.payload.email
-})
-    }
-})
+    extraReducers: (builder) => {
+        builder
+            .addCase(registerUser.pending, (state) => {
+                ((state.loading = true), (state.error = null));
+            })
+            .addCase(registerUser.fulfilled, (state, action) => {
+                ((state.loading = false),
+                    (state.step = "otp"),
+                    (state.cooldown = 60),
+                    (state.email = action.payload.email));
+            })
+            .addCase(verifyOtp.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(verifyOtp.fulfilled, (state, action) => {
+                ((state.loading = false), (state.user = action.payload.user), (state.step = "verified"));
+            })
+            .addCase(verifyOtp.rejected, (state, action) => {
+                ((state.loading = false), (state.error = action.payload));
+            });
+    },
+});
+
+export const {clearError,tickCooldown,resetAuthFlow,startCooldown} = authSlice.actions;
+export default authSlice.reducer;
