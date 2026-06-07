7fb7905c46d13b052d5353080e65393e06d03833
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-06-07 21:00:14 +0530

    fix: make nodemailer compatible with any SMTP service

--- server/utils/nodemailer.js
+++ server/utils/nodemailer.js
@@ -1,16 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT) || 465, // or 587 false
-  secure: Number(process.env.SMTP_PORT) === 465, // true for 465, false for 587
+  port: Number(process.env.SMTP_PORT) || 465,
+  secure: Number(process.env.SMTP_PORT) === 465,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
   },
   pool: true,
-  tls: { rejectUnauthorized: false } //for dev only
+  tls: { rejectUnauthorized: false }
 });
 const sendOtpEmail = async (email, otp, type = "verification") => {
   const isReset = type === "reset";
