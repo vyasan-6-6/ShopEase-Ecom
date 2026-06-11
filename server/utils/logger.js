28c0888a8806821ff10466b8679a48d5472df975
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-06-11 16:50:45 +0530

    Format logger.js

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -3,41 +3,41 @@ require("winston-daily-rotate-file");
 const path = require("path");
 
 const logFormat = winston.format.combine(
-    winston.format.errors({stack:true}),
+    winston.format.errors({ stack: true }),
     winston.format.json()
 );
 
 const infoTransport = new winston.transports.DailyRotateFile({
-    filename:path.join(__dirname,"../logs/info-%DATE%.log"),
-    datePattern:'YYY-MM-DD',
-    zippedArchive:true,
-    maxSize:"20m",
-    maxFiles:"14d",
-    level:"info",
-    format:logFormat
+    filename: path.join(__dirname, "../logs/info-%DATE%.log"),
+    datePattern: 'YYY-MM-DD',
+    zippedArchive: true,
+    maxSize: "20m",
+    maxFiles: "14d",
+    level: "info",
+    format: logFormat
 });
 
-const errorTranspot  = new winston.transports.DailyRotateFile({
-    filename:path.join(__dirname,"../logs/error-%DATE%.log"),
-    datePattern:'YYY-MM-DD',
-    zippedArchive:true,
-    maxSize:"20m",
-    maxFiles:"14d",
-    level:"error",
-    format:logFormat
+const errorTranspot = new winston.transports.DailyRotateFile({
+    filename: path.join(__dirname, "../logs/error-%DATE%.log"),
+    datePattern: 'YYY-MM-DD',
+    zippedArchive: true,
+    maxSize: "20m",
+    maxFiles: "14d",
+    level: "error",
+    format: logFormat
 });
 
 const consoleTransport = new winston.transports.Console({
-    format:winston.format.combine(
+    format: winston.format.combine(
         winston.format.colorize(),
         winston.format.simple(),
     )
 });
 
 const logger = winston.createLogger({
-    level:process.env.NODE_ENV === "production" ? "info" : "debug" , 
-    format:logFormat,
-    transports:[
+    level: process.env.NODE_ENV === "production" ? "info" : "debug",
+    format: logFormat,
+    transports: [
         infoTransport,
         errorTranspot,
         consoleTransport
